076bee6d480737ca6aaaaef81bd7c51b67c98b7e
Lucas Nishimura <lucas@nishisan.dev>
2025-12-12 12:17:02 -0300

    Refactor email address

--- src/main/java/dev/nishisan/utils/ngrid/replication/ReplicationHandler.java
+++ src/main/java/dev/nishisan/utils/ngrid/replication/ReplicationHandler.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright (C) 2020-2025 Lucas Nishimura <lucas.nishimura@gmail.com>
+ *  Copyright (C) 2020-2025 Lucas Nishimura <lucas.nishimura at gmail.com>
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
